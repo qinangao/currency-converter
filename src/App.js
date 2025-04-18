f1a394230c3c3496b04ad5b7c77710413e0f1e19
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-18 21:15:58 +0100

    create currency converter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("EUR");
+  const [result, setResult] = useState("");
+
+  useEffect(
+    function () {
+      async function converter() {
+        if (!input || from === to) return;
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${input}&from=${from}&to=${to}`
+        );
+
+        const data = await res.json();
+
+        setResult(data.rates[to]);
+      }
+      converter();
+    },
+    [input, from, to]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(Number(e.target.value))}
+      />
+      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={to} onChange={(e) => setTo(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        Result: {result} {to}
+      </p>
     </div>
   );
 }
