3a8a7d1dd5f015a5c5f0837e655974c92a61b3e6
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-18 21:22:28 +0100

    add isLoading state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(1);
   const [from, setFrom] = useState("USD");
   const [to, setTo] = useState("EUR");
   const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
       async function converter() {
-        if (!input || from === to) return;
+        setIsLoading(true);
         const res = await fetch(
           `https://api.frankfurter.app/latest?amount=${input}&from=${from}&to=${to}`
         );
@@ -17,7 +18,9 @@ function App() {
         const data = await res.json();
 
         setResult(data.rates[to]);
+        setIsLoading(false);
       }
+      if (from === to) return setResult(input);
       converter();
     },
     [input, from, to]
@@ -28,14 +31,23 @@ function App() {
         type="text"
         value={input}
         onChange={(e) => setInput(Number(e.target.value))}
+        disabled={isLoading}
       />
-      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+      <select
+        value={from}
+        onChange={(e) => setFrom(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={to} onChange={(e) => setTo(e.target.value)}>
+      <select
+        value={to}
+        onChange={(e) => setTo(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
